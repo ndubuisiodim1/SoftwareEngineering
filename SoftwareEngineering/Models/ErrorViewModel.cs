dcf948e3b5232dcf37d3b7958d20f2d5bbfc065b
ndubu <ndubu@DESKTOP-FDN1R1M>
2025-10-26 18:51:35 +0000

    Add project files.

---
+++ SoftwareEngineering/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoftwareEngineering.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
